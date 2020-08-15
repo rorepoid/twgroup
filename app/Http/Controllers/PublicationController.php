49db8e55b47b331cf6a0ef98e763d431d504d331
Roberto <rorepoid@gmail.com>
2020-08-15 13:50:31 -0500

    style: Remove some unnecesary code comments

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -7,77 +7,36 @@ use Illuminate\Http\Request;
 
 class PublicationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('publications.index', ['publications' => Publication::paginate(10)]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Publication  $publication
-     * @return \Illuminate\Http\Response
-     */
     public function show(Publication $publication)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Publication  $publication
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Publication $publication)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Publication  $publication
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Publication $publication)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Publication  $publication
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Publication $publication)
     {
         //
